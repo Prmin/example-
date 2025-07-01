7ab414ff0d2980fb80d2196f60e0b5633267d9e1
PraminO <owen63100@.com>
2025-07-01 13:10:04 +0700

    อัปเดตหน้า flex

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,9 @@ export default function HomePage() {
   return (
     // flex-1 ทำให้ <main> ขยายเต็มพื้นที่ที่เหลือระหว่าง header และ footer
     <main className="flex flex-1 p-4 gap-4">
-      <PostContent />
       <Sidebar />
+      <PostContent />
+      
     </main>
   );
 }
