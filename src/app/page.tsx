e41325dac730d2587e2ecd7bebd38cc2cf1be860
PraminO <owen63100@.com>
2025-07-01 12:47:43 +0700

    อัปเดตหน้า homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import PostContent from './components/PostContent';
 export default function HomePage() {
   return (
     // flex-1 ทำให้ <main> ขยายเต็มพื้นที่ที่เหลือระหว่าง header และ footer
-    <main className="flex flex-1 p-8 gap-8">
+    <main className="flex flex-1 p-4 gap-4">
       <PostContent />
       <Sidebar />
     </main>
